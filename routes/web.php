1f3ac7a5ba1df08eaec3295b4ccfe73dc68aeec7
anyuzhe <68067348@qq.com>
2019-07-25 15:08:15 +0800

    add course_jump

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,14 @@ Route::get('/test', function () {
     DB::getDoctrineColumn('users', 'id')->getType()->getName();
 });
 
+Route::get('/course/jump', function () {
+    requireMoodleConfig();
+    global $CFG;
+    $token = \App\Repositories\TokenRepository::getToken();
+    $course_id = request('course_id');
+    getCurl($CFG->wwwroot."/webservice/rest/server.php?wsfunction=enrol_self_enrol_user&wstoken={$token}&courseid={$course_id}&moodlewsrestformat=json");
+    return redirect($CFG->wwwroot.'/login/course_jump.php?token='.$token."&course_id=".$course_id);
+});
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
