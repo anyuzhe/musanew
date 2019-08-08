e3ecdd56b4fa44159a8ec4ddd141995f89b1adff
anyuzhe <68067348@qq.com>
2019-08-08 14:59:21 +0800

    add 集团

---
+++ app/Models/Conglomerate.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Conglomerate extends Model
+{
+    protected $table = 'conglomerates';
+
+    public $connection = 'musa';
+
+    public $fillable = [
+        'name',
+    ];
+}
