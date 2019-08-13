6e9a4298587a2e320bf7cbeb77e786c6898558d3
anyuzhe <68067348@qq.com>
2019-08-13 10:27:16 +0800

    edit job api for address

--- app/Repositories/JobsRepository.php
+++ app/Repositories/JobsRepository.php
@@ -13,15 +13,19 @@ class JobsRepository
         $data->load('address');
         $area_ids = [];
         foreach ($data as $v) {
-            $area_ids[] = $v->address->province_id;
-            $area_ids[] = $v->address->city_id;
-            $area_ids[] = $v->address->district_id;
+            if($v->address){
+                $area_ids[] = $v->address->province_id;
+                $area_ids[] = $v->address->city_id;
+                $area_ids[] = $v->address->district_id;
+            }
         }
         $areas = Area::whereIn('id', $area_ids)->get()->keyBy('id')->toArray();
         foreach ($data as &$v) {
-            $v->address->province_text = isset($areas[$v->province_id])?$areas[$v->province_id]['cname']:'';
-            $v->address->city_text = isset($areas[$v->city_id])?$areas[$v->city_id]['cname']:'';
-            $v->address->district_text = isset($areas[$v->district_id])?$areas[$v->district_id]['cname']:'';
+            if($v->address){
+                $v->address->province_text = isset($areas[$v->province_id])?$areas[$v->province_id]['cname']:'';
+                $v->address->city_text = isset($areas[$v->city_id])?$areas[$v->city_id]['cname']:'';
+                $v->address->district_text = isset($areas[$v->district_id])?$areas[$v->district_id]['cname']:'';
+            }
             getOptionsText($v);
             foreach ($v->skills as &$skill) {
                 $skill->skill_level = $skill->pivot->skill_level;
@@ -43,10 +47,12 @@ class JobsRepository
             $skill->used_time = $skill->pivot->used_time;
             getOptionsText($skill);
         }
-        $areas = Area::whereIn('id', [$data->address->province_id, $data->address->city_id, $data->address->district_id])->get()->keyBy('id')->toArray();
-        $data->address->province_text = isset($areas[$data->province_id])?$areas[$data->province_id]['cname']:'';
-        $data->address->city_text = isset($areas[$data->city_id])?$areas[$data->city_id]['cname']:'';
-        $data->address->district_text = isset($areas[$data->district_id])?$areas[$data->district_id]['cname']:'';
+        if($data->address){
+            $areas = Area::whereIn('id', [$data->address->province_id, $data->address->city_id, $data->address->district_id])->get()->keyBy('id')->toArray();
+            $data->address->province_text = isset($areas[$data->province_id])?$areas[$data->province_id]['cname']:'';
+            $data->address->city_text = isset($areas[$data->city_id])?$areas[$data->city_id]['cname']:'';
+            $data->address->district_text = isset($areas[$data->district_id])?$areas[$data->district_id]['cname']:'';
+        }
         return $data;
     }
 }
