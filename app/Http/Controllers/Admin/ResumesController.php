1056a35957f6c850f441e0c70c5373d82406e5f6
anyuzhe <68067348@qq.com>
2019-08-21 17:35:40 +0800

    test dump pdf

--- app/Http/Controllers/Admin/ResumesController.php
+++ app/Http/Controllers/Admin/ResumesController.php
@@ -26,6 +26,7 @@ class ResumesController extends Controller
 
     public function dumpPdf($id)
     {
+        global $CFG;
         requireMoodleConfig();
         $moodleRoot = getMoodleRoot();
 
@@ -46,7 +47,22 @@ class ResumesController extends Controller
         } else {
             shell_exec("/usr/local/bin/wkhtmltopdf $html $path"); // linux  wkhtmltopdf 调用
         }
-
+//文件输出浏览器下载
+        function upload($path, $pdfName)
+        {
+            if (!file_exists($path)) {
+                echo '文件不存在';
+                exit;
+            }
+            $filename = realpath($path); //文件名
+            Header("Content-type:  application/octet-stream ");
+            Header("Accept-Ranges:  bytes ");
+            Header("Accept-Length: " . filesize($filename));
+            header("Content-Disposition:  attachment;  filename= $pdfName");
+            echo file_get_contents($filename);
+            readfile($filename);
+            unlink($path);
+        }
         upload($path, $pdfName);
     }
 }
