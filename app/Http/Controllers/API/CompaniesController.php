3c915c3cd3cbe925491bdbb41ed297ee61618f00
anyuzhe <68067348@qq.com>
2019-08-28 14:43:53 +0800

    edit backlog 待面试

--- app/Http/Controllers/API/CompaniesController.php
+++ app/Http/Controllers/API/CompaniesController.php
@@ -315,18 +315,17 @@ class CompaniesController extends ApiBaseCommonController
         }
 
         //待面试
-        $demandSideIds = $company->demandSides()->pluck('company.id')->toArray();
-        $recruitIds = Entrust::whereIn('third_party_id', $demandSideIds)->pluck('company_job_recruit_id')->toArray();
+        $recruitIds = Entrust::whereIn('third_party_id', $company->id)->pluck('company_job_recruit_id')->toArray();
         $recruitIds = array_merge($recruitIds, Recruit::where('company_id', $company->id)->pluck('id')->toArray());
         $waitInterviewData = RecruitResume::where(function ($query)use($user, $company){
             $query->where('status',2)
-                ->whereIn('id',RecruitResumeLog::where('user_id',$user->id)->where('company_id',$company->id)->where('status',2)->pluck('company_job_recruit_resume_id')->toArray());
+                ->whereIn('id',RecruitResumeLog::where('user_id',$user->id)->where('status',2)->pluck('company_job_recruit_resume_id')->toArray());
         })->orWhere(function ($query)use($user, $company){
             $query->where('status',3)
-                ->whereIn('id',RecruitResumeLog::where('user_id',$user->id)->where('company_id',$company->id)->where('status',3)->pluck('company_job_recruit_resume_id')->toArray());
+                ->whereIn('id',RecruitResumeLog::where('user_id',$user->id)->where('status',3)->pluck('company_job_recruit_resume_id')->toArray());
         })->orWhere(function ($query)use($user, $company){
             $query->where('status',5)
-                ->whereIn('id',RecruitResumeLog::where('user_id',$user->id)->where('company_id',$company->id)->where('status',5)->pluck('company_job_recruit_resume_id')->toArray());
+                ->whereIn('id',RecruitResumeLog::where('user_id',$user->id)->where('status',5)->pluck('company_job_recruit_resume_id')->toArray());
         })->where('status',2)->whereIn('id', $recruitIds)->get();
         $waitInterviewData->load('job');
         $waitInterviewData->load('resume');
