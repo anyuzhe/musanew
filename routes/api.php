769b20716e9cd4be3c8bfeb8e3e4f5f52504bb70
zhenglong <68067348@qq.com>
2019-09-19 09:50:25 +0800

    上传简历

--- routes/api.php
+++ routes/api.php
@@ -83,9 +83,6 @@ $router->any('/user/findpassword/edit', 'API\LoginController@editPassword');
 //图片上传
 $router->post('/upload', 'Voyager\VoyagerController@uploadNew');
 
-//简历上传
-$router->post('/company/resume/upload', 'API\EntrustResumesController@upload');
-
 $router->group(['middleware' => 'auth.api'], function () use ($router) {
     $router->get('/company/jobs/idName', 'API\JobsController@allListIdName');
 
@@ -141,6 +138,10 @@ $router->group(['middleware' => 'auth.api'], function () use ($router) {
     $router->post('/company/entrusts/reject', 'API\EntrustsController@rejectEntrust');
 
     //简历
+
+//简历上传
+    $router->post('/company/resume/upload', 'API\EntrustResumesController@upload');
+
 //    $router->get('/user/resume', 'ResumesController@view');
     //列表
     $router->get('/company/resumes', 'API\EntrustResumesController@index');
