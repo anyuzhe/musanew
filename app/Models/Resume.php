1558a76eee61550fc4f2bfc7203e560c15132f25
zhenglong <68067348@qq.com>
2019-11-04 09:17:45 +0800

    fix 简历bug

--- app/Models/Resume.php
+++ app/Models/Resume.php
@@ -42,9 +42,11 @@ class Resume extends Model
         'on_the_job_company_name',//在职公司
         'is_public',//是否公开
         'is_used',//是否使用
+        'is_personal',//是否个人的简历
+        'is_base',//是否基础信息
        'resume_name',//简历名称
         'usable_range',//使用范围
-//        is_personal,is_base
+//        is_personal,
     ];
 
     public function skills()
