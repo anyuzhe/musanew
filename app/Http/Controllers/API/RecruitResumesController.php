744ec33536c50017b8d850c212a9a52b10c4ec76
zhenglong <68067348@qq.com>
2019-11-13 14:21:40 +0800

    同上

--- app/Http/Controllers/API/RecruitResumesController.php
+++ app/Http/Controllers/API/RecruitResumesController.php
@@ -98,9 +98,9 @@ class RecruitResumesController extends ApiBaseCommonController
         $data->logs->load('creatorInfo');
         foreach ($data->logs as &$log) {
             if($log->creatorInfo->avatar){
-                $log->creatorInfo->avatar = getPicFullUrl($log->creatorInfo->avatar);
+                $log->creatorInfo->avatar_url = getPicFullUrl($log->creatorInfo->avatar);
             }else{
-                $log->creatorInfo->avatar = "";
+                $log->creatorInfo->avatar_url = "";
             }
         }
         if($this->getCurrentCompany())
