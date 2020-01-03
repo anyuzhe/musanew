6715058f8b6ac4a1cb4e9711a9205415302d5029
zhenglong <68067348@qq.com>
2020-01-03 16:04:01 +0800

    edit 测试简历上传网页

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,7 +1,7 @@
 <html>
 <body>
 
-<form action="/api/upload" method="post" enctype="multipart/form-data">
+<form action="{!! url('/api/upload') !!}" method="post" enctype="multipart/form-data">
     <input type="file" name="file">
     <button type="submit">ok</button>
 </form>
