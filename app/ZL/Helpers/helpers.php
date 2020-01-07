5dadd372312564b2a46c0de7d5354389a3c2c544
zhenglong <68067348@qq.com>
2020-01-07 09:56:03 +0800

    同上一次提交

--- app/ZL/Helpers/helpers.php
+++ app/ZL/Helpers/helpers.php
@@ -872,7 +872,9 @@ function getCompanyRole($company, $user=null)
 
 function getCompanyRoles($company, $user=null)
 {
-    $company_role_id = CompanyUserRole::where('user_id', $user->id)->where('company_id', $company->id)->pluck('role_id');
+    $user_id = $user->id;
+    $company_id = $company->id;
+    $company_role_id = CompanyUserRole::where('user_id', $user_id)->where('company_id', $company_id)->pluck('role_id');
     $roles = CompanyRole::whereIn('id', $company_role_id)->get();
     return $roles;
 }
