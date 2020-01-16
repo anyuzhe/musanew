786cba3b0bb655643eae254a5bebbba08429497a
zhenglong <68067348@qq.com>
2020-01-16 15:30:32 +0800

    同上

--- app/Http/Controllers/API/CompaniesController.php
+++ app/Http/Controllers/API/CompaniesController.php
@@ -524,7 +524,7 @@ class CompaniesController extends ApiBaseCommonController
             $model = $model->where('department_id', $department_id);
         }
         $model1 = clone $model;
-        $companyUsers = $model->where('company_id', $company_id)->skip($pageSize*($pagination-1))->take($pageSize)->orderByRaw("FIELD(company_role_id, 1) asc")->get();
+        $companyUsers = $model->where('company_id', $company_id)->skip($pageSize*($pagination-1))->take($pageSize)->orderByRaw("FIELD(company_role_id, 1) desc")->get();
         $count = $model1->where('company_id', $company_id)->count();
         $companyUsers->load('department');
         $userIds = $companyUsers->pluck('user_id')->toArray();
