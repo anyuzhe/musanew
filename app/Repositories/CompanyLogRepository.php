d4ab19d76a73696da9929e7069012b91ad3b08d3
zhenglong <68067348@qq.com>
2020-03-05 15:22:52 +0800

    测试 job crud日志

---
+++ app/Repositories/CompanyLogRepository.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Area;
+
+class AreaRepository
+{
+    public static function getTree()
+    {
+        $all = Area::all()->toArray();
+        $data = [];
+        foreach ($all as $v) {
+            if($v['pid']==0){
+                self::getChild($v, $all);
+                $data[] = $v;
+//                Area::where('id', $v['id'])->update(['fname'=>$v['cname'],'level'=>1]);
+            }
+        }
+        return $data;
+    }
+
+    protected static function getChild(&$v, $all)
+    {
+        $v['children'] = [];
+        foreach ($all as $item) {
+            if($item['pid']==$v['id']){
+                $item['fname'] = $v['fname'].$item['cname'];
+                $item['level'] = $v['level']+1;
+//                Area::where('id', $item['id'])->update(['fname'=>$item['fname'], 'level'=>$item['level']]);
+                self::getChild($item, $all);
+                $v['children'][] = $item;
+            }
+        }
+        if(count($v['children'])==0){
+            $v['children'] = null;
+        }
+    }
+}
